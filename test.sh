372b7ca140c9c217c13ba088ef9948546ed0919b
Lukas Zilka <lukas@zilka.me>
2025-11-02 22:54:18 +0530

    Fixes bug where files deleted in upstream weren't properly deleted locally after mpull and kept lingering forever.

--- test.sh
+++ test.sh
@@ -12,41 +12,75 @@ git init -b main
 echo hello > hello.txt
 mkdir -p a/b/c/
 echo hi > a/b/c/hi.txt
+mkdir -p some/never/touched/
+echo never > some/never/touched/file.txt
 git add .
 git commit -m "initial commit"
 
 cd /tmp/gg-test-repo
-git clone /tmp/gg-test-repo-upstream .
-git mpull
-echo -e "hello\nmy change" > hello.txt
-
+    git clone /tmp/gg-test-repo-upstream .
+    git mpull
+    echo -e "hello\nmy change" > hello.txt
 
 cd /tmp/gg-test-repo-upstream
-echo -e "hello\nupstream change" > hello.txt
-git commit -am "The change."
+    echo -e "hello\nupstream change" > hello.txt
+    git commit -am "The change."
 
 cd /tmp/gg-test-repo
-git mpull
+    git mpull
 
-echo "# some_branch
-hello.txt
+    echo "# some_branch" >> .branches.txt
+    echo "hello.txt" >> .branches.txt
+    echo "" >> .branches.txt
+    echo "# another_branch" >> .branches.txt
+    echo "a/b/c/hi.txt" >> .branches.txt
+    echo "a/b/c/hi2.txt" >> .branches.txt
+    echo "" >> .branches.txt
 
-# another_branch
-a/b/c/hi.txt
-a/b/c/hi2.txt
-" > .branches.txt
-echo hi2 > a/b/c/hi2.txt
+    echo hi2 > a/b/c/hi2.txt
 
-git mpush some_branch
+    git mpush some_branch
 
-echo -e "something new" >> hello.txt
-echo -e "something new2" > hello2.txt
+    echo -e "something new" >> hello.txt
+    echo -e "something new2" > hello2.txt
 
-echo "# some_branch
-hello2.txt" >> .branches.txt
+    echo "# some_branch" >> .branches.txt
+    echo "hello2.txt" >> .branches.txt
 
-git mpush some_branch
-git mpush another_branch
+    git mpush some_branch
+    git mpush another_branch
 
-echo "xxx" >> hello2.txt
-git mpush some_branch
+    echo "xxx" >> hello2.txt
+    git mpush some_branch
+
+# Merge all into main.
+cd /tmp/gg-test-repo-upstream
+    git merge some_branch -m "Merge PR some_branch"
+    git merge another_branch -m "Merge PR another_branch"
+
+cd /tmp/gg-test-repo
+    git mpull
+
+# Test that files get deleted correctly when they are deleted from upstream.
+cd /tmp/gg-test-repo-upstream
+    rm some/never/touched/file.txt
+    rm hello.txt
+    git commit -am "Remove file."
+
+cd /tmp/gg-test-repo
+    git mpull
+    [ ! -e some/never/touched/file.txt ] || exit 1
+    [ ! -e hello.txt ] || exit 1
+
+# Test that a file that has local modifications won't get deleted
+# even when upstream deletes it.
+cd /tmp/gg-test-repo-upstream
+    rm hello2.txt
+    git commit -am "Remove file 2."
+
+cd /tmp/gg-test-repo
+    echo "This should not get deleted" > hello2.txt
+
+    git mpull
+
+    [ -e hello2.txt ] || exit 1
